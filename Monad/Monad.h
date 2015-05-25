2aee96fcff266f967245408e11aab1a09bdc4b32
Davide Mendolia <idaf1er@gmail.com>
2015-05-25 13:34:00 +0200

    Initial Commit

---
+++ Monad/Monad.h
@@ -0,0 +1,19 @@
+//
+//  Monad.h
+//  Monad
+//
+//  Created by Davide Mendolia on 25/5/15.
+//  Copyright (c) 2015 Karumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Monad.
+FOUNDATION_EXPORT double MonadVersionNumber;
+
+//! Project version string for Monad.
+FOUNDATION_EXPORT const unsigned char MonadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Monad/PublicHeader.h>
+
+
